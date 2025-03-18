4529609390fffddfa4fa72bc5b6d2a09b0b31e98
Mackenzie455 <168576512+Mackenzie455@users.noreply.github.com>
2025-03-18 10:25:59 +0000

    Changes to RPGClassLibrary.Combat

--- RPGClassLibrary/Actors/Enemy.cs
+++ RPGClassLibrary/Actors/Enemy.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPGClassLibrary.Actors;
+using RPGClassLibrary.Mechanics;
+using RPGClassLibrary.Items;
 
 namespace RPGClassLibrary.Actors
 {
@@ -14,7 +17,23 @@ namespace RPGClassLibrary.Actors
 		DarkMage,
 		Goblin
 	}
-	public class Enemy
+	public class Enemy : Entity
 	{
+		public Enemy(string name, Role role, EntityStats stats, int[] statDist) : base(name, role)
+		{
+			Stats = stats;
+			Money = 100;
+			StatDistribution = statDist;
+			AllocationPoints = 20;
+			Inventory = new List<Item>();
+		}
+
+		public static void EnemyFactory(int enemyDifficulty, int playerLevel)
+		{
+			//generate enemy name
+			//generate enemy role
+			//generate enemy stats and statDist
+			//scales with enemyDifficulty, GlobalDifficulty and the players level
+		}
 	}
 }
---
+++ RPGClassLibrary/Combat/CombatHandler.cs
@@ -0,0 +1,19 @@
+using RPGClassLibrary.Actors;
+using RPGClassLibrary.Operations;
+
+namespace RPGClassLibrary.Combat
+{
+    public class CombatHandler
+    {
+        public static void BattleStarter(Player p)
+        {
+            //create enemy(Enemy.EnemyFactory())
+            //pass current player and enemy into battle method (CombatHandler.BattleLogic(Player, enemy)
+        }
+
+        public static void BattleLogic(Enemy e, Player p)
+        {
+            
+        }
+    }
+}
