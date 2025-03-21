b471ca14d49a36d1b04b516aa79b09a8ee374112
Mackenzie Roberts <mackenzieg544@gmail.com>
2025-03-21 20:01:34 +0000

    TurnHandler and BattleModule created.

---
+++ RPGClassLibrary/Combat/BattleModule.cs
@@ -0,0 +1,17 @@
+﻿using RPGClassLibrary.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGClassLibrary.Combat
+{
+	public class BattleModule
+	{
+		static void TakeDamage() { }
+		static void Attack(Entity target) { }
+		static void Defend(Entity target) { }
+		static void ConsumeItem(Entity target) { }
+	}
+}
