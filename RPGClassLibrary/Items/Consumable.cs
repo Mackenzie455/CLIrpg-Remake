e0b79e1f6719351f8a94dd073e0cd5694b8d5c82
Mackenzie Roberts <mackenzieg544@gmail.com>
2025-03-21 20:02:00 +0000

    Consumable class created.

---
+++ RPGClassLibrary/Items/Consumable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGClassLibrary.Items
+{
+	public class Consumable : Item
+	{
+		public Consumable(int iD, string name, ItemRarity rarity, ItemType type) : base(iD, name, rarity, type)
+		{
+
+		}
+	}
+}
